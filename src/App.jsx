acdbc200995ddf80a98ba767cec5acc4317f5561
ChalalLyna <kyysmkym@gmail.com>
2024-02-24 10:19:51 +0100

    notications_page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home'
 import LoginSign from './pages/LoginSign'
 import Games from './pages/Games'
+import Notifications from './pages/Notifications'
 
 import './styles.css'
 
@@ -14,6 +15,7 @@ export default function App() {
         <Route exact path="/" element={<LoginSign />} />
         <Route exact path="/home" element={<Home />} />
         <Route exact path="/games" element={<Games />} />
+        <Route exact path="/notifications" element={<Notifications />} />
       </Routes>
     </Router>
   )
